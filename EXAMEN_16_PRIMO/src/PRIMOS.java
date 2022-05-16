b97759b860ad76e299eb5afdbf2fd80ab941e3e7
Lara <Lara@LAPTOP-L23FN667>
2022-05-16 09:33:10 +0200

    Introducir número por pantalla

---
+++ EXAMEN_16_PRIMO/src/PRIMOS.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class PRIMOS {
+
+	public static void main(String[] args) {
+		
+		Scanner ent = new Scanner(System.in);
+
+		System.out.println("Buenos días Ceinmark, soy Lara Chiquito");
+
+		System.out.println("Introduce un número para saber si es primo o no:");
+
+
+	}
+
+}
